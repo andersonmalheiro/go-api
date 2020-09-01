5ccc009bad2775c1370c2da52aa50079a36bc290
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2020-08-31 22:55:24 -0300

    Updating user interface

--- domain/entities/user/interface.go
+++ domain/entities/user/interface.go
@@ -1,8 +1,10 @@
 package user
 
+import "gorm.io/gorm"
+
 // IUser interface defines the methods that User repository must implement
 type IUser interface {
-	Add(*User) error
+	Add(*User, *gorm.DB) error
 	Update(*User) error
 	Delete(int64) error
 	Get(*User) error
